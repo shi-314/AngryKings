89694e31180d32be8a84646a4c4eef83d8d08fe3
Johann <git@johann-hofmann.com>
2013-07-04 23:36:07 +0200

    took lobby entirely out of history

--- src/com/angrykings/activities/MainActivity.java
+++ src/com/angrykings/activities/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends Activity {
 				Intent intent = new Intent(getApplicationContext(),
 						LobbyActivity.class);
 				intent.putExtra("username", username);
-				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
 				startActivity(intent);
 			}
 

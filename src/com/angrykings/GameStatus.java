d3a88b3ef2a97d773d83637b41599f9d96bd1921
shi-314 <zn31415926535@gmail.com>
2013-11-21 16:49:42 +0100

    new class to save the game status

---
+++ src/com/angrykings/GameStatus.java
@@ -0,0 +1,14 @@
+package com.angrykings;
+
+/**
+ * GameStatus
+ *
+ * @author Shivan Taher <zn31415926535@gmail.com>
+ * @date 21.11.13
+ */
+public enum GameStatus {
+	MY_TURN,
+	PARTNER_TURN,
+	WON,
+	LOST
+}

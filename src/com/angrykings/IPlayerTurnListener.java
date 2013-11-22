fc2756562156b550f20265701483febd83e1a84a
shi-314 <zn31415926535@gmail.com>
2013-11-22 01:47:03 +0100

    new interface to handle specific player turn events

---
+++ src/com/angrykings/IPlayerTurnListener.java
@@ -0,0 +1,14 @@
+package com.angrykings;
+
+/**
+ * IPlayerTurnListener
+ *
+ * @author 	Shivan Taher <zn31415926535@gmail.com>
+ * @date 	21.11.13
+ */
+public interface IPlayerTurnListener {
+
+	void onHandleTurn();
+	void onEndTurn();
+
+}

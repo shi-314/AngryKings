7a468ae62cbf7f080bd43c11f1bc9d05f5e00ef8
shi-314 <zn31415926535@gmail.com>
2014-02-06 23:22:38 +0100

    Use simple vibration pattern

--- src/com/angrykings/gcm/GcmIntentService.java
+++ src/com/angrykings/gcm/GcmIntentService.java
@@ -1,16 +1,23 @@
 package com.angrykings.gcm;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.angrykings.R;
+import com.angrykings.activities.MainActivity;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GcmIntentService extends IntentService {
-    public static final int NOTIFICATION_ID = 1;
+    public static int NOTIFICATION_ID = 1;
     private NotificationManager mNotificationManager;
     NotificationCompat.Builder builder;
 
@@ -32,8 +39,35 @@ public class GcmIntentService extends IntentService {
 
             String message = extras.getString("msg");
             Log.i("GCM", "receive: " + message);
+
+            sendNotification(this, "AngryKings", message);
         }
 
         GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    public static void sendNotification(Context context, String title, String msg) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // TODO: Set game id or something on the intent and handle it in MainActivity
+
+        PendingIntent intent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+
+        Notification.Builder notificationBuilder =
+                new Notification.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(msg)
+                        .setStyle(new Notification.BigTextStyle().bigText(msg));
+
+        notificationBuilder.setContentIntent(intent);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        notificationBuilder.setSound(alarmSound);
+
+        long[] pattern = {0,250,0};
+        notificationBuilder.setVibrate(pattern);
+
+        nm.notify(NOTIFICATION_ID++, notificationBuilder.build());
     }
 }

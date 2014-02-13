c51d6757385070e4daba659b4f1cb28c4882be90
shi-314 <zn31415926535@gmail.com>
2014-02-13 04:48:16 +0100

    Load profile images in fb list

--- src/com/angrykings/pregame/FacebookPlayerAdapter.java
+++ src/com/angrykings/pregame/FacebookPlayerAdapter.java
@@ -2,6 +2,12 @@ package com.angrykings.pregame;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -75,5 +81,28 @@ public class FacebookPlayerAdapter extends BaseAdapter{
         ImageLoader.getInstance().displayImage(profilePicture, fb);
 
         return vi;
+    }
+
+    public static Bitmap circularCrop(Bitmap bitmap) {
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+
+        paint.setColor(color);
+
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2, bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+
     }
 }

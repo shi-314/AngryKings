96bf43b8551e1e4dc4461c18802165fffb818a1d
johann <git@johann-hofmann.com>
2014-08-24 12:12:03 +0200

    add apk deploy script

---
+++ deployApk.sh
@@ -0,0 +1,2 @@
+cp ./out/production/AngryKings/AngryKings.apk ./out/production/AngryKings/angrykings.apk
+rsync -v -u ./out/production/AngryKings/angrykings.apk spaeti@spaeti.pavo.uberspace.de:/home/spaeti/html/
